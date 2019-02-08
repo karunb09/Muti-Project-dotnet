d76c6d5c18e7ebf6ce85cc086a7f6e6b4206cdb9
karun <11141770+karunb09@users.noreply.github.com>
2019-02-08 15:07:48 -0600

    Unittests added

--- Multiprjct/tests/UnitTests/UnitTest1.cs
+++ Multiprjct/tests/UnitTests/UnitTest1.cs
@@ -6,9 +6,56 @@ namespace UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Add_WorksWithTwoPositiveIntegers()
         {
+            var expected = 0;
+            var a = 2;
+            var b = 2;
+            var actual = Mod(a, b);
+            Assert.Equal(expected, actual);
+        }
 
+        [Fact]
+        public void Add_WorksWithTwoNegativeIntegers()
+        {
+            var expected = -1;
+            var a = -5;
+            var b = -2;
+            var actual = Mod(a, b);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(9)]
+        public void IsOdd_WorksWithManyInputs(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+
+        [Theory]
+        [InlineData(0, 2, 2)]
+        [InlineData(-2, -7, -5)]
+        [InlineData(0, 2, -2)]
+        public void Add_WorksWithManyInputs(int exp, int n1, int n2)
+        {
+            Assert.Equal(exp, Mod(n1, n2));
+        }
+
+
+
+        // for more see https://xunit.github.io/docs/getting-started/netcore/cmdline
+
+        int Mod(int x, int y)
+        {
+            return x % y;
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
         }
     }
 }
